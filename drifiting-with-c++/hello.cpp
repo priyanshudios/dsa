56acac441af45e3311d189a0d62a9efafb83eca7
Priyanshu <prynsu@yahoo.com>
2026-01-28 21:03:10 +0530

    Basic data types

--- drifiting-with-c++/hello.cpp
+++ drifiting-with-c++/hello.cpp
@@ -2,9 +2,11 @@
 using namespace std; // helping us to emint std before every 
 
 int main () {
-    int balance = 999999999
-    float poor = 9.99999999
-    double rich = 99999999.9
+    int balance = 999999999; // whole numbers
+    float poor = 9.99999999; // decimal
+    double rich = 99999999.9; // decimal
+    char character = "p"; // single character
+    bool legal = true; // boolean
     cout << "Hello" << endl;
     cout << balance << endl;
     cout << 20 + 3 << endl;
@@ -13,3 +15,4 @@ int main () {
     cin.get();
     return 0;
 }
+
