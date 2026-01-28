2a5a3454457f5e5d1c8fdaae38c0da897e6b5f8f
Priyanshu <prynsu@yahoo.com>
2026-01-28 21:00:08 +0530

    integer variables

--- drifiting-with-c++/hello.cpp
+++ drifiting-with-c++/hello.cpp
@@ -2,7 +2,14 @@
 using namespace std; // helping us to emint std before every 
 
 int main () {
+    int balance = 999999999
+    float poor = 9.99999999
+    double rich = 99999999.9
     cout << "Hello" << endl;
+    cout << balance << endl;
+    cout << 20 + 3 << endl;
+    cout << 20 - 3 << endl;
+    cout << 20 * 3 << endl;
     cin.get();
     return 0;
 }
