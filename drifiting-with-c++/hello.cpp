3da892fffe47b262040bcecc1ab688160636e8e1
Priyanshu <prynsu@yahoo.com>
2026-01-28 18:48:42 +0530

    My first C program

---
+++ drifiting-with-c++/hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // header file which is helping us to use cout
+using namespace std; // helping us to emint std before every 
+
+int main () {
+    cout << "Hello" << endl;
+    cin.get();
+    return 0;
+}
