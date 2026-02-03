e57690e8f7b82e85fc9d6b03836b4efcdc6807b6
Priyanshu <74850965+priyanshudios@users.noreply.github.com>
2026-02-03 18:06:54 +0000

    Loops

---
+++ loops/print-1-n.cpp
@@ -0,0 +1,14 @@
+// Print 1 to n
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    int n;
+    int i = 1;
+    cin >> n;
+    while (i <= n) {
+        cout << i << endl;
+        i++;
+    }
+}
---
+++ loops/print-n-1.cpp
@@ -0,0 +1,13 @@
+// Print n to 1
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    int n;
+    cin >> n;
+    while (n >= 1) {
+        cout << n << endl;
+        n--;
+    }
+}
